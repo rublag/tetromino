1b9ed930d4a49ae73ae3d3658a41c0a74c5375fe
rublag <rublag-ns@yandex.ru>
2015-09-01 03:16:08 +0900

    Partial GameController realisation

--- beta-tetramino.js
+++ beta-tetramino.js
@@ -1,4 +1,4 @@
-function Game()
+function Tetris()
 {
     /* Tetris API */
     var gameController = new GameController();
@@ -21,19 +21,43 @@ function Game()
 
 function GameController()
 {
-    this.field = new Field();
+    // Connects browser and game
+    this.state = "unready";
 
-    function startGame()
+    this.prepare = function()
     {
-        field.clear();
+        this.field = new Field();
         this.terominoes = new Tetrominoes();
         this.brick_generator = new BrickGenerator(this.terominoes);
-        this.game_field = new GameField(this.brick_generator);
+        this.game = new Game(this.brick_generator);
+        this.state = "prepared"
+    };
 
-    }
-
-    function stopGame()
+    this.startGame = function()
     {
+        field.clear();
+        this.game.setCallback(this.game.fieldUpdated, this.updateField(this.game.gameField));
+        this.game.setCallback(this.game.gameEnded, this.gameOver());
+        this.game.prepare();
+        this.game.start();
+        this.state = "started"
+    };
 
-    }
+    this.stopGame = function()
+    {
+        this.game.stop();
+        this.game.removeCallbacks();
+        this.state = "stopped";
+    };
+
+    this.pauseGame = function()
+    {
+        this.game.pause();
+        this.state = "stopped";
+    };
+}
+
+function Game()
+{
+    // Tetris game itself. Doesn't require any
 }
